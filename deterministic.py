3e42ec42ea718fb055abb3bb3c6f57ede03e28de
Gerrit Nowald <gerrit.nowald@continental-corporation.com>
2022-06-27 09:52:33 +0200

    create initial population

--- deterministic.py
+++ deterministic.py
@@ -1,14 +1,46 @@
+"""
+Simulation of demographic development in Banished
+
+author: Gerrit Nowald
+"""
+
 import random
 
+#------------------------------------------------------------------------------
+# parameters
+
 InitialPopulation = 8
+InitialAge = 20
+
+#------------------------------------------------------------------------------
+# classes
 
 class citizen():
-    def __init__(self):
-        self.age = 0
-        if random.randint(0,1) == 0:
-            self.female = True
+    def __init__(self, age=0, female=None):
+        self.age    = age
+        self.female = female
+        if female == None:
+            # define sex randomly
+            if random.randint(0,1) == 0:
+                self.female = True
+            else:
+                self.female = False
+
+#------------------------------------------------------------------------------
+# create start population
 
 citizens = set()
 
 for n in range(InitialPopulation):
-    citizens.add(citizen())
+    # define sex alternating
+    if n % 2 == 0:
+        female = True
+    else:
+        female = False
+    # add new citizen 
+    citizens.add( citizen(age=InitialAge, female=female) )
+
+#------------------------------------------------------------------------------
+# 
+
+PopulationFemale = [ citizen.female for citizen in citizens ]
